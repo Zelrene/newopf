1fc55df5ac249660494898d5a057ec387ca8ee87
melissaflores417 <melissaflores417@nevada.unr.edu>
2022-03-06 12:27:58 -0800

    create tickets view

--- view.py
+++ view.py
@@ -1,9 +1,32 @@
+from turtle import title
 from markupsafe import escape
-from flask import Flask, abort
-
+from flask import Flask, abort, render_template, request, redirect
+from src import controller
 app = Flask(__name__)
 
+
 @app.route('/')
-@app.route('/index/')
-def hello():
-	return '<h1>Hello, World!</h1>'
+def index():
+	return render_template('index.html')
+
+@app.route('/create_tickets.html', methods = ['GET', 'POST'])
+def create_tickets():
+	if request.method == 'GET':
+		return render_template('create_tickets.html')
+	
+	if request.method == 'POST':
+		Title = request.form['Title']
+		Description = request.form['Description']
+		Location = request.form['Location']
+		Building = request.form['Building']
+		Unit = request.form['Unit#']
+		Contact = request.form['Contact']
+		AdditonalNotes = request.form['AdditionalNotes']
+
+		return redirect('/')
+
+
+
+@app.route('/view_tickets.html')
+def view_tickets():
+	return render_template('view_tickets.html', tickets = tickets)
