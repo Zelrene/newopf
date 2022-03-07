e7b2bd588245f833b4c488ab3008c7121aad528a
njuana <njuana@nevada.unr.edu>
2022-03-06 19:21:13 -0800

    included the add_ticket funtion to db_connector.py

--- src/db_connector.py
+++ src/db_connector.py
@@ -1,3 +1,4 @@
+from multiprocessing import connection
 from src.models import ticket
 from view import db
 
@@ -5,10 +6,11 @@ class DB_Connector():
     def __init__(self):
         db.create_all()
 
-    '''
+    
     def add_ticket(self, id, status, severity_level, description, building, unit, title, location, additionalNotes, contact):
         new_ticket = ticket(id, status, severity_level, description, building, unit, title, location, additionalNotes, contact)
         db.session.add(new_ticket)
         db.session.commit(new_ticket)
-    '''
     
+
+
