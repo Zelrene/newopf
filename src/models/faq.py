0395fd2cfde8332d3871c953f86f2ed9a23e5e4a
njuana <njuana@nevada.unr.edu>
2022-03-13 01:50:13 -0800

    created the faq model

---
+++ src/models/faq.py
@@ -0,0 +1,12 @@
+from view import db
+
+
+class Faq(db.Model):
+    __tablename__ = 'faq'
+    id = db.Column(db.Integer, primary_key = True)
+    question = db.Column(db.Text, nullable = False)
+    answer = db.Column(db.Text, nullable = True)
+
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
