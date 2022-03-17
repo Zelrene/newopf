b887d8e0f23b63912786c9b3f926c862910c3fd5
njuana <njuana@nevada.unr.edu>
2022-03-16 20:08:44 -0700

    chagned the user model for roles

--- src/models/user.py
+++ src/models/user.py
@@ -1,7 +1,8 @@
 from flask_login import UserMixin
+#from sqlalchemy import PrimaryKeyConstraint
 from view import db
 
-
+'''
 # Define the UserRoles association table
 class UserRoles(db.Model):
     __tablename__ = 'user_roles'
@@ -49,3 +50,42 @@ class Role(db.Model):
         self.name = name
 
 
+'''
+
+#creating a new user and role model
+
+#Define the User data_model
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(150), nullable = False)
+    last_name = db.Column(db.String(150), nullable = False)
+    user_role = db.Column(db.String(50), nullable = False)
+    contact_email = db.Column(db.String(120), nullable = False, unique = True)
+    net_id = db.Column(db.String(50), nullable = False, unique = True)
+    gender = db.Column(db.Enum('M', 'F', 'NA'), nullable = False)
+    student_year = db.Column(db.String(50))
+    password = db.Column(db.String(500), nullable = False)
+
+    # Define the user role relationship to Role via UserRoles
+    #roles = db.relationship('Role', backref='user', lazy ='dynamic')
+
+    # Define the user ticket relationship to Role via tickets
+    # a user can have multiple tickets (one to many relationstip)
+    tickets = db.relationship('Ticket', backref= 'user', lazy = True)
+
+    def __init__(self, first_name, last_name, user_role, contact_email, net_id, gender, student_year, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_role = user_role
+        self.contact_email = contact_email
+        self.net_id = net_id
+        self.gender = gender
+        self.student_year = student_year
+        self.password = password
+    
+
+    def get_id(self):
+        return self.id
+
+    
