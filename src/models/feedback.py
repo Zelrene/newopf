6680b5c4e4a94cf9e2472695ab1c6bce925c1978
melissaflores417 <melissaflores417@nevada.unr.edu>
2022-04-10 17:16:33 -0700

    created feedback view and form

--- src/models/feedback.py
+++ src/models/feedback.py
@@ -7,12 +7,12 @@ class Feedback(db.Model):
     __tablename__ = 'feedback'
     id = db.Column(db.Integer, primary_key = True)
     ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'), nullable = False, unique = True)
-    experience_rate = db.Column(db.Integer, nullable = False)
+    experience_rate = db.Column(db.Text, nullable = False)
     satisfied_level = db.Column(db.Enum('Yes', 'No', 'Could improve'), nullable = False)
     additional_comments = db.Column(db.Text, nullable = False)
 
     
-    def __init__(self, ticekt_id, experience_rate, satisfied_level, additional_comments):
+    def __init__(self, ticket_id, experience_rate, satisfied_level, additional_comments):
         self.ticket_id = ticket_id
         self.experience_rate = experience_rate
         self.satisfied_level = satisfied_level
